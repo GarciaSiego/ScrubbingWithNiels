74d716f08da970395869718daec976506edbb784
Garcia <gsrijnders@hotmail.com>
2018-09-07 17:05:08 +0200

    fix teacher mistakes

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,20 @@ import com.jaunt.*;
 public class Main {
 
     public static void main(String[] args) throws ResponseException {
-//        UserAgent userAgent = new UserAgent();
-//        userAgent.visit("https://www.detelefoongids.nl/pharmades-apotheek-hoogerheide/11421611/5-1/");                        //visit a url
-//        System.out.println(userAgent.doc.innerHTML());
-        run();
-    }
-
-    private static void run(){
-        try{
+        try {
             UserAgent userAgent = new UserAgent();                       //create new userAgent (headless browser).
             for (int i = 1; i < 2; i++) {
                 userAgent.visit("https://www.zorgkaartnederland.nl/zorginstelling/pagina" + i + "?sort=naam-asc");
                 Elements divs = userAgent.doc.findEach("<div class=\"media-body\">");
-                for(Element div : divs){
+                for (Element div : divs) {
                     Element p = div.findFirst("<p class=\"description\">");
                     System.out.println("Title:" + p.getChildText());//join child text of body element
                     // get url link
-                    Element href = div.findFirst("<a href=\"");
+//                    Element href = div.findFirst("<a href=\"");
 
                 }
             }
-        }
-        catch(JauntException e){         //if an HTTP/connection error occurs, handle JauntException.
+        } catch (JauntException e) {         //if an HTTP/connection error occurs, handle JauntException.
             System.err.println(e);
         }
     }
